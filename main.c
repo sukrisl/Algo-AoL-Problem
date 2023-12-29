a53fb659c40f5601b87e73f03ff7be6021a5fc0a
sukrisl <laksono.sukri@gmail.com>
2023-12-29 18:58:54 +0700

    refactor: add header file for program definitions

--- main.c
+++ main.c
@@ -3,37 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_DATA_COUNT 10000
-
-#define MENU_DISPLAY 1
-#define MENU_SEARCH 2
-#define MENU_SORT 3
-#define MENU_EXPORT 4
-#define MENU_EXIT 5
-
-#define SEARCH_LOCATION 0
-#define SEARCH_CITY 1
-#define SEARCH_PRICE 2
-#define SEARCH_ROOMS 3
-#define SEARCH_BATHROOM 4
-#define SEARCH_PARK 5
-#define SEARCH_TYPE 6
-#define SEARCH_FURNISH 7
-
-#define SORT_ASCENDING 0
-#define SORT_DESCENDING 1
-
-typedef struct {
-    char location[100];
-    char city[100];
-    int price, rooms, bathroom, park;
-    char type[100];
-    char furnish[100];
-} Data_t;
-
-typedef bool (*CompareFunction)(Data_t* a, Data_t* b);
-typedef bool (*MatchStrFunction)(Data_t* data, const char* str);
-typedef bool (*MatchIntFunction)(Data_t* data, const int number);
+#include "defs.h"
 
 Data_t list_data[MAX_DATA_COUNT];
 Data_t export_list_data[MAX_DATA_COUNT];
